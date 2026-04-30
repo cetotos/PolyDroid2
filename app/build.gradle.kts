c65e32e12db3bb9e300d2051afeb0caebc0d9134
cetotos <cetotospatatos@gmail.com>
2026-04-30 14:15:19 +0300

    bump to 0.8.1b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "0.8.0b"
+        versionName = "0.8.1b"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += "arm64-v8a" }
     }
